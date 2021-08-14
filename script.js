564e923bb404670c8a77ec0d0c79d6721ed321d4
derotto54 <jason.ganiatsas@gmail.com>
2021-08-14 14:27:24 -0400

    remove items from list

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ function genList() {
     todoList.innerHTML=''
     for (var i = 0; i < todos.length; i++ ) {
     var addLi = document.createElement('li')
+    addLi.setAttribute("dataIndex", i)
     addLi.innerText = todos[i]
+    var button = document.createElement('button')
+    button.innerHTML = '&#10004'
+    addLi.appendChild(button)
     todoList.appendChild(addLi)
     }
 }
@@ -28,4 +32,19 @@ function addTodo(event) {
     newTodoInput.value =''
     genList()
 }
+
+function removeTodo(event) {
+    console.log(event.target)
+    if (event.target.tagName == 'BUTTON' ) {
+        var idx = event.target.parentElement.getAttribute('dataIndex')
+    console.log(idx)
+    todos.splice(idx,1)
+    console.log(todos)
+    localStorage.setItem('todos', JSON.stringify(todos))
+    genList()
+
+    }
+}
+
+todoList.addEventListener('click', removeTodo)
 newTodoForm.addEventListener('submit',addTodo)
