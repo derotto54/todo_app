5ac9561f043c353b2d0b47e6c646c32f1af3e2ca
derotto54 <jason.ganiatsas@gmail.com>
2021-08-14 12:26:24 -0400

    html skeleton

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("poop")
+
+
+
+
+// Add a todo item and a button to enter the information
+
+
+// Count the number of todos
+
+
+//List all todos
+
+
+//Add a button to mark as complete for each listed todo and remove it from storage
